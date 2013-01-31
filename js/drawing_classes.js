d36d484ccc58d3fd85e7c7ddc238f7afb0b12371
Hilmar Kári Hallbjörnsson <zormoni@gmail.com>
2013-01-31 09:29:40 +0000

    Core things completed
    Can do all the core things plus one bonus point

--- js/drawing_classes.js
+++ js/drawing_classes.js
@@ -2,23 +2,29 @@
 //parameter and it implements the before function which is always the same in
 //all the drawing objects.
 var Shape = Base.extend({
-  constructor: function(ctx) {
-  	this.context = ctx;
+	constructor: function(ctx) {
+		this.context = ctx;
+		this.points = [];
   },
-  	before: function(x,y){
-  		this.startX = x;
-  		this.startY = y;
-  	}
+	before: function(x,y){
+		this.startX = x;
+		this.startY = y;
+	}
 });
 
+//------------------------LINE-----------------------------------------------
 //Draws a single line between the starting and the ending points
-var Line = Shape.extend({ 
+var Line = Shape.extend({
+	name: function(){
+		return "line";
+	},
+
 	//The draw function draws the "temporary" object to the screen.  While the mouse
 	//is moving and we're in drawing mode, we draw the object for the user to have
 	//some reference on what he's drawing.
-	draw: function(x,y) {
+	draw: function(event, x,y) {
 		this.context.beginPath(); // starts the line drawing
-		this.context.moveTo(this.startX,this.startY);	
+		this.context.moveTo(this.startX,this.startY);
 		this.context.lineTo(x,y);
 		this.context.lineWidth=1;
 		this.context.strokeStyle = "grey";
@@ -28,9 +34,9 @@ var Line = Shape.extend({
 	//This is the function that persists the drawed object on the canvas
 	//It also returns a literal with the information about the object that
 	//was drawn for history purposes
-	after: function(x,y){
+	after: function(event, x,y){
 		this.context.beginPath(); // starts the line drawing
-		this.context.moveTo(this.startX,this.startY);	
+		this.context.moveTo(this.startX,this.startY);
 		this.context.lineTo(x,y);
 		this.context.lineWidth=3;
 		this.context.strokeStyle = "red";
@@ -39,44 +45,90 @@ var Line = Shape.extend({
 			type: "line",
 			points: [this.startX,this.startY,x,y],
 			width: 3,
-			color: "red"
+			color: "red",
+			time: Date.now()
 		};
 	},
 
 	//Gets an object from the history and re-draws it self to the canvas
 	redraw: function(object){
 		this.context.beginPath(); // starts the line drawing
-		this.context.moveTo(object.points[0], object.points[1]);	
+		this.context.moveTo(object.points[0], object.points[1]);
 		this.context.lineTo(object.points[2], object.points[3]);
 		this.context.lineWidth=object.width;
 		this.context.strokeStyle = object.color;
 		this.context.stroke(); // Draws the actual outline of the line
 	}
 });
+//------------------------RECTANGLE-----------------------------------------------
+var Rectangle = Shape.extend({ // instance interface
+	/*constructor: function(x, y, width, height, color) {
+		this.base(x, y, color);
+		this.width = width;
+		this.height = height;
+	},*/
+	name: function(){
+		return "rectangle";
+	},
 
+	draw: function(event, x,y) {
+		this.context.strokeStyle = "grey";
+		this.context.strokeRect(this.startX, this.startY, x-this.startX, parseInt((this.startY - y)*-1, 10) );
+	
+		//filling a rectangle
+		//context.fillStyle = this.color;
+		//context.fillRect(this.x, this.y, this.width, this.height);
+		
+		//drawing the outline
+		//context.strokeStyle = "black";
+		//context.strokeRect(this.x, this.y, this.width, this.height);
+	},
+
+	after: function(event, x,y){
+		this.context.strokeStyle = "black";
+		this.context.strokeRect(this.startX, this.startY, x-this.startX, parseInt((this.startY - y)*-1, 10) );
+
+		return {
+			type: "rectangle",
+			points: [ this.startX, this.startY, x-this.startX, parseInt((this.startY - y)*-1, 10) ],
+			width: 3,
+			color: "black",
+			time: Date.now()
+		};
+	},
+
+	redraw: function(object){
+		this.context.strokeStyle = object.color;
+		this.context.strokeRect(object.points[0], object.points[1], object.points[2], object.points[3] );
+	}
+});
+//------------------------CIRCLE-----------------------------------------------
 //Draws and circle from the starting point with a radius of half of where the ending
 //points are
 var Circle = Shape.extend({ // instance interface
-	radius: 0,
-  
-	getCircumference: function() {
-		return 2 * Circle.PI * this.radius;
+	name: function(){
+		return "circle";
+	},
+
+	getCircumference: function(x,y) {
+		return Math.sqrt(
+				Math.pow(y - startY, 2)+
+				Math.pow(x - startX, 2)
+			);
 	},
 
 	//The draw function draws the "temporary" object to the screen.  While the mouse
 	//is moving and we're in drawing mode, we draw the object for the user to have
 	//some reference on what he's drawing.
-	draw: function(x,y) {
+	//@todo: Circle is strange at the beginning of the drawing
+	draw: function(event, x,y) {
 		//setting the radius by using Pythagoras and dx and dy for the a's and b's
 		//then we divide by two to get half of the distance as radius
-		this.radius = parseInt(Math.sqrt( Math.pow(this.startX-x,2)/Math.pow(this.startY-y,2))/2);  
-		console.log(this.radius);
+		var radius = this.getCircumference(x,y);
 		//drawing a filled circle with outline:
 		this.context.beginPath();
-		this.context.arc( this.startX, this.startY, this.radius, 0, 2 * Math.PI, false);
+		this.context.arc( this.startX, this.startY, radius, 0, 2 * Math.PI, false);
 		// param doc http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas-element.html#dom-context-2d-arc
-		this.context.fillStyle = this.color;
-		this.context.fill();
 		this.context.lineWidth = 1;
 		this.context.strokeStyle = "grey";
 		this.context.stroke();
@@ -85,117 +137,166 @@ var Circle = Shape.extend({ // instance interface
 	//This is the function that persists the drawed object on the canvas
 	//It also returns a literal with the information about the object that
 	//was drawn for history purposes
-	after: function(x,y){
-		
-		return { type: "circle" };
+	after: function(event, x,y){
+		var radius = this.getCircumference(x,y);
+		//drawing a filled circle with outline:
+		this.context.beginPath();
+		this.context.arc( this.startX, this.startY, radius, 0, 2 * Math.PI, false);
+		this.context.lineWidth = 2;
+		this.context.strokeStyle = "blue";
+		this.context.stroke();
+		return { type: "circle",
+				points: [this.startX, this.startY, radius],
+				width: 2,
+				color: "blue",
+				time: Date.now()
+				};
 	},
 
 	//Gets an object from the history and knows how to re-draw it self to the canvas
 	redraw: function(object){
-		console.log("Im an unimplemented redraw-circle()");
-	}
-});
-
-
-var Rectangle = Shape.extend({ // instance interface
-	/*constructor: function(x, y, width, height, color) {
-		this.base(x, y, color);
-		this.width = width;
-		this.height = height;
-	},*/
-
-	draw: function(context) {
-		//filling a rectangle
-		context.fillStyle = this.color;
-		context.fillRect(this.x, this.y, this.width, this.height);
-		
-		//drawing the outline
-		context.strokeStyle = "black";
-		context.strokeRect(this.x, this.y, this.width, this.height);
-	}
-});
-
-
-var Line = Shape.extend({ // instance interface
-	/*constructor: function(x, y, width, height,  color) {
-		this.base(x, y, color);
-		this.width = width;
-		this.height = height;
-	},*/
-  
-  	
-
-	draw: function(x,y) {
-		this.context.beginPath(); // starts the line drawing
-		this.context.moveTo(this.startX,this.startY);	
-		this.context.lineTo(x,y);
-		this.context.lineWidth=1;
-		this.context.strokeStyle = "grey";
+		this.context.beginPath();
+		this.context.arc( object.points[0], object.points[1], object.points[2], 0, 2 * Math.PI, false);
+		this.context.lineWidth = object.width;
+		this.context.strokeStyle = object.color;
 		this.context.stroke();
+	}
+});
+
+//------------------------CHARACTERS-----------------------------------------------
+var Characters = Shape.extend({
+	name: function(){
+		return "characters";
 	},
 
-	after: function(x,y){
-		this.context.beginPath(); // starts the line drawing
-		this.context.moveTo(this.startX,this.startY);	
-		this.context.lineTo(x,y);
-		this.context.lineWidth=3;
-		this.context.strokeStyle = "red";
-		this.context.stroke(); // Draws the actual outline of the line
-		return {
-			type: "line",
-			points: [this.startX,this.startY,x,y],
-			width: 3,
-			color: "red"
+	draw: function(x,y){
+
+	},
+
+	//@todo: Display dialog at cursor pointer and give it focus
+	openDialog: function(){
+		$("#textbox").css('visibility', 'visible');
+	},
+
+	closeDialog: function(){
+		$("#freetext").val("");
+		$("#textbox").css("visibility", "hidden");
+	},
+
+	after: function(text){
+		this.context.font = '12pt Arial';
+		this.context.fillStyle = 'black';
+		this.context.fillText(text, this.startX, this.startY);
+		this.context.strokeStyle = 'black';
+		this.context.lineWidth = 1;
+		this.context.strokeText(text, this.startX, this.startY);
+		return { type: "characters",
+			points: [this.startX, this.startY],
+			text: text,
+			color: "black",
+			width: 1,
+			time: Date.now()
 		};
 	},
 
-	//Redraws a line object from the history array
 	redraw: function(object){
-		this.context.beginPath(); // starts the line drawing
-		this.context.moveTo(object.points[0], object.points[1]);	
-		this.context.lineTo(object.points[2], object.points[3]);
-		this.context.lineWidth=object.width;
+		this.context.font = '12pt Arial';
+		this.context.fillStyle = object.color;
+		this.context.fillText(object.text, object.points[0], object.points[1]);
 		this.context.strokeStyle = object.color;
-		this.context.stroke(); // Draws the actual outline of the line
+		this.context.lineWidth = 1;
+		this.context.strokeText(object.text, object.points[0], object.points[1]);
 	}
 });
 
+//------------------------MATH SYMBOLS-----------------------------------------
+var MathSymbols = Shape.extend({
+	name: function(){
+		return "mathsymbols";
+	},
 
-var Characters = Shape.extend({ // instance interface
-	/*constructor: function(x, y, color) {
-		this.base(x, y, color);
-	},*/
-  
-	draw: function(context) {	
-		context.font = '30pt Helvetica';
-		context.fillStyle = this.color;
-		context.fillText('Hello World!', this.x, this.y);
-		context.strokeStyle = 'black';
-		context.lineWidth = 1;
-		context.strokeText('Hello World!', this.x, this.y);
+	openDialog: function(){
+		$("#mathbox").css('visibility', 'visible');
+	},
+
+	closeDialog: function(){
+		$("#mathformula").val("");
+		$("#mathbox").css('visibility', 'hidden');
+	},
+
+	after: function(text){
+		var imageObj = new Image();
+		imageObj.src = "http://latex.codecogs.com/gif.latex?" + text;
+		this.context.drawImage(imageObj, this.startX, this.startY);
+		return {
+			type: "mathsymbols",
+			points: [this.startX, this.startY],
+			formula: text,
+			time: Date.now()
+		};
+	},
+
+	redraw: function(object){
+		var imageObj = new Image();
+		imageObj.src = "http://latex.codecogs.com/gif.latex?" + object.formula;
+		this.context.drawImage(imageObj, object.points[0], object.points[1]);
 	}
 });
 
-
+//------------------------PENCIL-----------------------------------------------
 var Pencil = Shape.extend({ // instance interface
-	/*constructor: function(coordinates, color) {
-		this.base(coordinates[0][0], coordinates[0][1], color);
-		this.coordinates = coordinates;
-	},*/
-  
-	draw: function(context) {
-		context.beginPath(); // starts the line drawing
-		//Loop through the coordinates and draw
-		for (var i = 0; i < this.coordinates.length; i++) {
-		  // Do something with element i.
-			context.moveTo(this.coordinates[i][0],this.coordinates[i][1]);	
-			if (i !== this.coordinates.length-1) {
-				context.lineTo(this.coordinates[i+1][0],this.coordinates[i+1][1]);				
-			};
-		}
-		context.lineWidth=3;
-		context.strokeStyle = this.color;
-		context.stroke(); // Draws the actual outline of the line
+	name: function(){
+		return "pencil";
+	},
+
+	draw: function(event, x,y) {
+		var self = this;
+		this.context.beginPath();
+		this.context.lineWidth = 1;
+		this.context.strokeStyle = "rgb(220,220,220)";
+		this.context.moveTo(this.startX, this.startY);
+		this.points.forEach(function(item,index,collection){
+			self.context.lineTo(item.x, item.y);
+		});
+		this.context.lineTo(x,y);
+		this.points.push( {x:x, y:y} );
+		this.context.stroke();
+	},
+
+	after: function(event, x,y){
+		var self = this;
+		this.context.beginPath();
+		this.context.lineWidth = 1;
+		this.context.strokeStyle = "orange";
+		this.context.moveTo(this.startX, this.startY);
+		this.points.forEach(function(item,index,collection){
+			self.context.lineTo(item.x, item.y);
+		});
+		this.context.lineTo(x,y);
+		this.points.push( {x:x, y:y} );
+		this.context.stroke();
+		var pointsClone = this.points.slice(0);
+		this.points = [];
+		return {
+			type: "pencil",
+			points: [this.startX, this.startY],
+			path: pointsClone,
+			color: "orange",
+			width: 1,
+			time: Date.now()
+		};
+	},
+
+	redraw: function(object){
+		var self = this;
+		this.context.beginPath();
+		this.context.lineWidth = object.width;
+		this.context.strokeStyle = object.color;
+		this.context.moveTo(object.points[0],object.points[1]);
+		object.path.forEach(function(item, index, collection){
+			self.context.lineTo(item.x, item.y);
+		});
+		this.context.stroke();
 	}
 });
 
--- js/main.js
+++ js/main.js
@@ -2,14 +2,15 @@
 	//Object variables.
 	//These three are used solely for displaying the co-ordinates of the mousepointer
 	//on the screen
-	var locat = document.getElementById('VTMouseLocation');
-	var mdown = document.getElementById('VTMouseDown');
-	var mup = document.getElementById('VTMouseUp');
+	var locat = document.getElementById('MouseLocation');
+	var tool = document.getElementById('Tool');
 	//These store if we are drawing or not and all the drawing objects on the screen
 	var drawingMode = false;
 	var drawingHistory = [];
+	var undoHistory = [];
+	var redoHistory = [];
 	//The context and the canvas itself
-	var c=document.getElementById("VTCanvas");
+	var c=document.getElementById("Canvas");
 	var ctx=c.getContext("2d");
 	
 	//Initializing the drawing objects.  Now we have them ready whenever we
@@ -19,7 +20,8 @@
 		"rectangle": new Rectangle(ctx),
 		"line": new Line(ctx),
 		"characters": new Characters(ctx),
-		"pencil": new Pencil(ctx)
+		"pencil": new Pencil(ctx),
+		"mathsymbols": new MathSymbols(ctx)
 	};
 
 	//Inintializing the current drawing object to a line
@@ -30,92 +32,139 @@
 	$("#line").click(function(event){
 		event.preventDefault();
 		currentDrawingObject = tools.line;
+		tool.innerText = "Tool: Line";
 	});
 
 	$("#circle").click(function(event){
 		event.preventDefault();
 		currentDrawingObject = tools.circle;
+		tool.innerText = "Tool: Circle";
 	});
 
-	//Clears the canvas and loops through all the drawing objects in the hisorty to 
+	$("#rectangle").click(function(event){
+		event.preventDefault();
+		currentDrawingObject = tools.rectangle;
+		tool.innerText = "Tool: Rectangle";
+	});
+
+	$("#pencil").click(function(event){
+		event.preventDefault();
+		currentDrawingObject = tools.pencil;
+		tool.innerText = "Tool: Pencil";
+	});
+
+	$("#characters").click(function(event){
+		event.preventDefault();
+		currentDrawingObject = tools.characters;
+		tool.innerText = "Tool: Strings";
+	});
+
+	$("#mathsymbols").click(function(event){
+		event.preventDefault();
+		currentDrawingObject = tools.mathsymbols;
+		tool.innerText = "Tool: Math (LaTeX)";
+	});
+
+	$("#undo").click(function(event){
+		if( drawingHistory.length > 0){
+			undoHistory.push( drawingHistory.pop() );
+			redraw();
+		}
+	});
+
+	$("#redo").click(function(event){
+		if( undoHistory.length > 0){
+			drawingHistory.push( undoHistory.pop() );
+			redraw();
+		}
+	});
+
+	//Clears the canvas and loops through all the drawing objects in the hisorty to
 	//redraw them on the canvas.
 	function redraw(){
 		ctx.clearRect(0,0,c.width, c.height);
 		drawingHistory.forEach(function(item, index, collection){
 			switch(item.type){
 				case "line": tools.line.redraw(item); break;
+				case "rectangle": tools.rectangle.redraw(item); break;
 				case "circle": tools.circle.redraw(item); break;
+				case "pencil": tools.pencil.redraw(item); break;
+				case "characters": tools.characters.redraw(item); break;
+				case "mathsymbols": tools.mathsymbols.redraw(item); break;
+				default:
+					break;
 			}
-			
 		});
-	};
+	}
 
 
 	//A heloer function to get the correct x and y coordinates from the client
 	function windowToCanvas(canvas, x, y){
 		var bbox = canvas.getBoundingClientRect();
-		return { x: parseInt(x - bbox.left * (canvas.width / bbox.width)),
-				 y: parseInt(y - bbox.top  * (canvas.height / bbox.height))
+		return { x: parseInt(x - bbox.left * (canvas.width / bbox.width), 10),
+				y: parseInt(y - bbox.top  * (canvas.height / bbox.height), 10)
 		};
-	};
+	}
 
-//MOUSE EVENTS --------------------------------------------------------	
+//MOUSE EVENTS --------------------------------------------------------
 	/**
-	 * Listening on mousedown event on #VTCanvas
+	 * Listening on mousedown event on #Canvas
 	 */
-	$("#VTCanvas").mousedown(function(e){
+	$("#Canvas").mousedown(function(e){
 		ctx.clearRect(0,0,c.width, c.height);
 		var loc = windowToCanvas(c, e.clientX, e.clientY);
 		drawingMode = true;
 		startX = e.clientX;
 		startY = e.clientY;
-		updateStart(loc.x, loc.y);
 		currentDrawingObject.before(loc.x, loc.y);
+		if(currentDrawingObject.name() === "characters" ){
+			currentDrawingObject.openDialog();
+			drawingMode = false;
+		}else if(currentDrawingObject.name() === "mathsymbols"){
+			currentDrawingObject.openDialog();
+			drawingMode = false;
+		}
 		redraw();
 	});
 
-	$("#VTCanvas").mousemove(function(e){
+	$("#Canvas").mousemove(function(e){
 		var loc = windowToCanvas(c, e.clientX, e.clientY);
 		updateLocation(loc.x, loc.y);
 		endX = e.clientX;
 		endY = e.clientY;
 		if(drawingMode === true){
 			redraw();
-			currentDrawingObject.draw(loc.x, loc.y);
+			currentDrawingObject.draw(e, loc.x, loc.y);
 		}
 	});
 
-	$("#VTCanvas").mouseup(function(e){
+	$("#Canvas").mouseup(function(e){
 		var loc = windowToCanvas(c, e.clientX, e.clientY);
-		updateFinish(loc.x, loc.y);
 		drawingMode = false;
-		drawingHistory.push(currentDrawingObject.after(loc.x, loc.y) );
-		console.log(drawingHistory);
+		if(currentDrawingObject.name() !== "characters" && currentDrawingObject.name() !== "mathsymbols"){
+			drawingHistory.push(currentDrawingObject.after(e, loc.x, loc.y) );
+		}
+	});
 
+	$("#textbox").keypress(function(e){
+		if( e.keyCode === 13){
+			drawingHistory.push(currentDrawingObject.after($("#freetext").val()) );
+			currentDrawingObject.closeDialog();
+		}
+	});
+
+	$("#mathbox").keypress(function(e){
+		if( e.keyCode === 13){
+			drawingHistory.push(currentDrawingObject.after($("#mathformula").val()) );
+			currentDrawingObject.closeDialog();
+			console.log(drawingHistory);
+		}
 	});
 
 	//These display the current mouse coordinates on the screen.
 	function updateLocation(x,y){
-		locat.innerText = 'Mouse is here: (' + x.toFixed(0) + ', ' + y.toFixed(0) + ')';
-	};
-
-	function updateStart(x,y){
-		mdown.innerText = 'Action started here: (' + x.toFixed(0) + ', ' + y.toFixed(0) + ')';
-	};
-
-	function updateFinish(x,y){
-		mup.innerText = 'Action ended here: (' + x.toFixed(0) + ', ' + y.toFixed(0) + ')';
-	};
-
-	//A function to undo the current action
-	function undo(){
-
-	};
-
-	//A function to redo the last action
-	function redo(){
-
-	};
+		locat.innerText = 'Coords: (' + x.toFixed(0) + ', ' + y.toFixed(0) + ')';
+	}
 })();
 
 
